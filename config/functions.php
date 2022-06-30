d133bd6b9878b4a7a56351bdc2ada79f245719a2
Antho <maxister@gmail.com>
2022-06-30 14:08:52 +0200

    comcom

--- config/functions.php
+++ config/functions.php
@@ -1,9 +1,13 @@
 <?php
+
 function defaultRSSArray() {
     //faire un tableau qui regroupe 2 flux global, 2 flux tennis et 2 flux foot et retourne ces flux sous forme de tableau.
     //les flux doivent comporter le titre, la date, la description avec l'image ainsi que l'url pour rediriger vers la news.
-    // {['titre', 'date', 'description', 'url'],
-    //['titre2', 'date2', 'description2', 'url2']}
+    
+    //{
+    //['titre', 'date', 'description', 'url'],
+    //['titre2', 'date2', 'description2', 'url2']
+    //}
     
     
     
@@ -13,8 +17,11 @@ function defaultRSSArray() {
 function customizedRSSArray($preference1, $preference2, $preference3, $nbFluxToShow) {
     //faire un tableau qui regroupe les flux en fonction des préférences utilisateurs ( type de flux et nombre)
     //Les flux doivent contenir comme pour le tableau par défaut 4 éléments : le titre, la date, la description ( qui inclu l'image) ainsi que l'url.
-    // {['titre', 'date', 'description', 'url'],
-    //['titre2', 'date2', 'description2', 'url2']}
+    
+    //{
+    //['titre', 'date', 'description', 'url'],
+    //['titre2', 'date2', 'description2', 'url2']
+    //}
 
 
 
