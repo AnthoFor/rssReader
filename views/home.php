fb8dc97327e62aaaa4c41faab40af63bbba8a0a0
Antho <maxister@gmail.com>
2022-07-01 16:15:16 +0200

    gestion des oneflux

--- views/home.php
+++ views/home.php
@@ -1,22 +1,22 @@
 <?php
 $defautArray = defaultRSSArray($arrayFoot, $arrayBasket, $arrayTennis);
 
-foreach ($defautArray as $value) {
-    echo '<p>'.$value[0].'</p>';
-    echo '<p>'.$value[1].'</p>';
-    echo '<p>'.$value[2].'</p>';
-    echo '<p>'.$value[3].'</p>';
-}
-
-$cat = 'foot';
-$oneCat = onlyOneFlux($cat, $megaArray);
-// foreach ($oneCat as $key => $value) {
+// foreach ($defautArray as $value) {
 //     echo '<p>'.$value[0].'</p>';
 //     echo '<p>'.$value[1].'</p>';
 //     echo '<p>'.$value[2].'</p>';
 //     echo '<p>'.$value[3].'</p>';
 // }
-
+if (!empty($_GET['cat'])) {
+    $cat = $_GET['cat'];
+    $oneCat = onlyOneFlux($cat, $megaArray);
+    foreach ($oneCat as $key => $value) {
+        echo '<p>'.$value[0].'</p>';
+        echo '<p>'.$value[1].'</p>';
+        echo '<p>'.$value[2].'</p>';
+        echo '<p>'.$value[3].'</p>';
+    }
+}
 $customizedArray = customizedRSSArray($megaArray, 12, 2, 0, 1);
 // foreach ($customizedArray as $key => $value) {
 //     echo '<p>'.$value[0].'</p>';
