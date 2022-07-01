8afa7530ae6aa447d651b5929869a093ef3b520d
Antho <maxister@gmail.com>
2022-07-01 16:24:03 +0200

    kikoo

--- views/home.php
+++ views/home.php
@@ -1,12 +1,15 @@
 <?php
-$defautArray = defaultRSSArray($arrayFoot, $arrayBasket, $arrayTennis);
-
-// foreach ($defautArray as $value) {
-//     echo '<p>'.$value[0].'</p>';
-//     echo '<p>'.$value[1].'</p>';
-//     echo '<p>'.$value[2].'</p>';
-//     echo '<p>'.$value[3].'</p>';
-// }
+//Si get est empty, gogo tableau par défaut ( foot + basket + tennis)
+if (empty($_GET)) {
+    $defautArray = defaultRSSArray($arrayFoot, $arrayBasket, $arrayTennis);
+    foreach ($defautArray as $value) {
+    echo '<p>'.$value[0].'</p>';
+    echo '<p>'.$value[1].'</p>';
+    echo '<p>'.$value[2].'</p>';
+    echo '<p>'.$value[3].'</p>';
+    }
+}
+//Si get['cat'] est pas vide, alors affiche le mono tableau
 if (!empty($_GET['cat'])) {
     $cat = $_GET['cat'];
     $oneCat = onlyOneFlux($cat, $megaArray);
@@ -17,6 +20,7 @@ if (!empty($_GET['cat'])) {
         echo '<p>'.$value[3].'</p>';
     }
 }
+
 $customizedArray = customizedRSSArray($megaArray, 12, 2, 0, 1);
 // foreach ($customizedArray as $key => $value) {
 //     echo '<p>'.$value[0].'</p>';
