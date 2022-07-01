5a52cf708e3a24efa7a0808d6a92fdd64bde79c2
Antho <maxister@gmail.com>
2022-07-01 16:42:00 +0200

    kikoo

--- views/home.php
+++ views/home.php
@@ -9,16 +9,28 @@ if (empty($_GET)) {
     echo '<p>'.$value[3].'</p>';
     }
 }
-//Si get['cat'] est pas vide, alors affiche le mono tableau
+
+//Si get['cat'] est pas vide ET valide alors affiche le mono tableau, sinon affiche le tableau par défaut
 if (!empty($_GET['cat'])) {
-    $cat = $_GET['cat'];
-    $oneCat = onlyOneFlux($cat, $megaArray);
-    foreach ($oneCat as $key => $value) {
+    $cat = trim(filter_input(INPUT_GET, 'cat', FILTER_SANITIZE_SPECIAL_CHARS));
+    if ($cat != 'global' && $cat != 'tennis' && $cat !='foot' && $cat !='basket' && $cat !='rugby' ){
+        $defautArray = defaultRSSArray($arrayFoot, $arrayBasket, $arrayTennis);
+        foreach ($defautArray as $value) {
         echo '<p>'.$value[0].'</p>';
         echo '<p>'.$value[1].'</p>';
         echo '<p>'.$value[2].'</p>';
         echo '<p>'.$value[3].'</p>';
+        }
+    } else {
+        $oneCat = onlyOneFlux($cat, $megaArray);
+        foreach ($oneCat as $key => $value) {
+        echo '<p>'.$value[0].'</p>';
+        echo '<p>'.$value[1].'</p>';
+        echo '<p>'.$value[2].'</p>';
+        echo '<p>'.$value[3].'</p>';
+        }
     }
+
 }
 
 $customizedArray = customizedRSSArray($megaArray, 12, 2, 0, 1);
