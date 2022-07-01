797aa46a04e843706fbac8f9ed07f5e1da54b7b5
Antho <maxister@gmail.com>
2022-07-01 14:26:48 +0200

    3 tableaux done

--- config/functions.php
+++ config/functions.php
@@ -1,46 +1,78 @@
 <?php
-function defaultRSSArray() {
-    //faire un tableau qui regroupe 2 flux global, 2 flux tennis et 2 flux foot et retourne ces flux sous forme de tableau.
-    //les flux doivent comporter le titre, la date, la description avec l'image ainsi que l'url pour rediriger vers la news.
-    
-    //{
-    //['titre', 'date', 'description', 'url'],
-    //['titre2', 'date2', 'description2', 'url2']
-    //}
-    
-    
-    
-    return;
+function defaultRSSArray($arrayFoot, $arrayBasket, $arrayTennis) {
+    $defautArray = array();
+    for ($i=0; $i < 6; $i++) {
+        array_push($defautArray,[$arrayFoot[$i][0], $arrayFoot[$i][1], $arrayFoot[$i][2], $arrayFoot[$i][3]]);
+        array_push($defautArray,[$arrayBasket[$i][0], $arrayBasket[$i][1], $arrayBasket[$i][2], $arrayBasket[$i][3]]); 
+        array_push($defautArray,[$arrayTennis[$i][0], $arrayTennis[$i][1], $arrayTennis[$i][2], $arrayTennis[$i][3]]);
+    }
+    return $defautArray;
 }
 
 /**
  * [Description for customizedRSSArray]
  *
- * @param object $preference1 fait référence au type de flux (global, foot, basket, tennis)
- * @param object $preference2 fait référence au type de flux (global, foot, basket, tennis)
- * @param object $preference3 fait référence au type de flux (global, foot, basket, tennis)
- * @param int $nbFluxToShow fait référence au type de flux (6, 9 ou 12)
+ * @param int $nbFluxToShowTotal $nbFluxToShowTotal fait référence au nombre de flux souhaité (6, 9 ou 12)
+ * @param int $nbTheme $nbTheme fait référence au nombre de theme voulu par l'utilisateur ( 2 ou 3)
+ * @param int $preference1 fait référence au type de flux (basket 0, foot 1, rugby 2, tennis 3, global 4)
+ * @param int $preference2 fait référence au type de flux (basket 0, foot 1, rugby 2, tennis 3, global 4)
+ * @param int $preference3 fait référence au type de flux (basket 0, foot 1, rugby 2, tennis 3, global 4)
  * 
- * @return array
+ * @return [type]
  * 
  */
-function customizedRSSArray(object $preference1, object $preference2, object $preference3, int $nbFluxToShow) {
-    //faire un tableau qui regroupe les flux en fonction des préférences utilisateurs ( type de flux et nombre)
-    //Les flux doivent contenir comme pour le tableau par défaut 4 éléments : le titre, la date, la description ( qui inclu l'image) ainsi que l'url.
-    
-    //{
-    //['titre', 'date', 'description', 'url'],
-    //['titre2', 'date2', 'description2', 'url2']
-    
-    //}
 
-
-
-
-
-    return;
+function customizedRSSArray ($megaArray, $nbFluxToShowTotal, int $nbTheme, int $preference1, int $preference2, int $preference3 = NULL) {
+    //$nbTheme = nombre de theme choisi par l'utilisateur, 2 ou 3. ( si il en selectionne 1, go go fonction onlyOneFlux)
+    //si 2 themes avec 9 flux, bah ca fera 4 et 4.
+    $nbFluxUnity = round($nbFluxToShowTotal / $nbTheme, 0, PHP_ROUND_HALF_UP); 
+    $customizedArray = array(); 
+        //preference 1
+    for ($i=0; $i < $nbFluxUnity; $i++) { 
+        if ($preference1 == 0) {
+            array_push($customizedArray, [$megaArray[0][$i][0], $megaArray[0][$i][1], $megaArray[0][$i][2], $megaArray[0][$i][3]]);
+        } elseif ($preference1 == 1) {
+            array_push($customizedArray, [$megaArray[1][$i][0], $megaArray[1][$i][1], $megaArray[1][$i][2], $megaArray[1][$i][3]]);
+        } elseif ($preference1 == 2) {
+            array_push($customizedArray, [$megaArray[2][$i][0], $megaArray[2][$i][1], $megaArray[2][$i][2], $megaArray[2][$i][3]]);
+        } elseif ($preference1 == 3) {
+            array_push($customizedArray, [$megaArray[3][$i][0], $megaArray[3][$i][1], $megaArray[3][$i][2], $megaArray[3][$i][3]]);
+        } elseif ($preference1 == 4) {
+            array_push($customizedArray, [$megaArray[4][$i][0], $megaArray[4][$i][1], $megaArray[4][$i][2], $megaArray[4][$i][3]]);
+        }
+    }
+    //préférence 2
+    for ($i=0; $i < $nbFluxUnity; $i++) {
+        if ($preference2 == 0) {
+            array_push($customizedArray, [$megaArray[0][$i][0], $megaArray[0][$i][1], $megaArray[0][$i][2], $megaArray[0][$i][3]]);
+        } elseif ($preference2 == 1) {
+            array_push($customizedArray, [$megaArray[1][$i][0], $megaArray[1][$i][1], $megaArray[1][$i][2], $megaArray[1][$i][3]]);
+        } elseif ($preference2 == 2) {
+            array_push($customizedArray, [$megaArray[2][$i][0], $megaArray[2][$i][1], $megaArray[2][$i][2], $megaArray[2][$i][3]]);
+        } elseif ($preference2 == 3) {
+            array_push($customizedArray, [$megaArray[3][$i][0], $megaArray[3][$i][1], $megaArray[3][$i][2], $megaArray[3][$i][3]]);
+        } elseif ($preference2 == 4) {
+            array_push($customizedArray, [$megaArray[4][$i][0], $megaArray[4][$i][1], $megaArray[4][$i][2], $megaArray[4][$i][3]]);
+        }
+    }
+    //preference 3
+    for ($i=0; $i < $nbFluxUnity; $i++) {
+        if (!empty($preference3)) {
+            if ($preference3 == 0) {
+                array_push($customizedArray, [$megaArray[0][$i][0], $megaArray[0][$i][1], $megaArray[0][$i][2], $megaArray[0][$i][3]]);
+            } elseif ($preference3 == 1) {
+                array_push($customizedArray, [$megaArray[1][$i][0], $megaArray[1][$i][1], $megaArray[1][$i][2], $megaArray[1][$i][3]]);
+            } elseif ($preference3 == 2) {
+                array_push($customizedArray, [$megaArray[2][$i][0], $megaArray[2][$i][1], $megaArray[2][$i][2], $megaArray[2][$i][3]]);
+            } elseif ($preference3 == 3) {
+                array_push($customizedArray, [$megaArray[3][$i][0], $megaArray[3][$i][1], $megaArray[3][$i][2], $megaArray[3][$i][3]]);
+            } elseif ($preference3 == 4) {
+                array_push($customizedArray, [$megaArray[4][$i][0], $megaArray[4][$i][1], $megaArray[4][$i][2], $megaArray[4][$i][3]]);
+            }
+        }
+    }
+    return $customizedArray;
 }
-
 
 /**
  * [Description for onlyOneFlux]
@@ -50,15 +82,30 @@ function customizedRSSArray(object $preference1, object $preference2, object $pr
  * @return array
  * 
  */
-function onlyOneFlux(string $categorie) {
+function onlyOneFlux(string $categorie, $megaArray) {
     //En fonction de quel sport l'utilisateur choisi, faire un foreach de la $var concernée ($rssFeedBasket,$rssFeedFoot, $rssFeedRugby, $rssFeedTennis, ...)
-
-
-
-
-    
-
-    return;
+    $oneCatArray = array();
+    if ($categorie == 'foot') {
+        for ($i=0; $i < 12; $i++) {   
+            array_push($oneCatArray,[$megaArray[1][$i][0], $megaArray[1][$i][1], $megaArray[1][$i][2], $megaArray[1][$i][3]]);
+        }
+    } elseif ($categorie == 'basket') {
+        for ($i=0; $i < 12; $i++) {  
+            array_push($oneCatArray,[$megaArray[0][$i][0], $megaArray[0][$i][1], $megaArray[0][$i][2], $megaArray[0][$i][3]]);          
+        }
+    } elseif ($categorie == 'rugby') {
+        for ($i=0; $i < 12; $i++) {       
+            array_push($oneCatArray,[$megaArray[2][$i][0], $megaArray[2][$i][1], $megaArray[2][$i][2], $megaArray[2][$i][3]]);
+        }
+    } elseif ($categorie == 'tennis') {
+        for ($i=0; $i < 12; $i++) {      
+            array_push($oneCatArray,[$megaArray[3][$i][0], $megaArray[3][$i][1], $megaArray[3][$i][2], $megaArray[3][$i][3]]);       
+        }
+    } elseif ($categorie == 'global') {
+        for ($i=0; $i < 12; $i++) {    
+            array_push($oneCatArray,[$megaArray[4][$i][0], $megaArray[4][$i][1], $megaArray[4][$i][2], $megaArray[4][$i][3]]);
+        }
+    }
+    return $oneCatArray;
 }
-
 ?>
--- controllers/pages-controller.php
+++ controllers/pages-controller.php
@@ -1,4 +1,5 @@
 <?php
+    require_once(__DIR__.'/../config/functions.php');
     //Sport global
     $rssFeedGlobal = new SimpleXMLElement(file_get_contents("https://rmcsport.bfmtv.com/rss/fil-sport/"));
     //RSS Basket
@@ -16,6 +17,8 @@
     $arrayFoot = array();
     $arrayRugby = array();
     $arrayTennis = array();
+    //agrégation de toute les autres arrays dans un mega array
+    $megaArray = array();
 
     foreach ($rssFeedGlobal->channel->item as $value) {
         array_push($arrayGlobal, [$value->pubDate, $value->title, $value->description, $value->link]);
@@ -32,58 +35,31 @@
     foreach ($rssFeedTennis->channel->item as $value) {
         array_push($arrayTennis, [$value->pubDate, $value->title, $value->description, $value->link]);
     }
+    array_push($megaArray, $arrayBasket, $arrayFoot, $arrayRugby, $arrayTennis, $arrayGlobal);
+    $defautArray = defaultRSSArray($arrayFoot, $arrayBasket, $arrayTennis);
+    foreach ($defautArray as $value) {
+        echo '<p>'.$value[0].'</p>';
+        echo '<p>'.$value[1].'</p>';
+        echo '<p>'.$value[2].'</p>';
+        echo '<p>'.$value[3].'</p>';
+    }
 
+    $cat = 'foot';
+    $oneCat = onlyOneFlux($cat, $megaArray);
+    // foreach ($oneCat as $key => $value) {
+    //     echo '<p>'.$value[0].'</p>';
+    //     echo '<p>'.$value[1].'</p>';
+    //     echo '<p>'.$value[2].'</p>';
+    //     echo '<p>'.$value[3].'</p>';
+    // }
 
-    //exploitation des diffférents tableaux
-    //tableau global
-    echo '<p>'.$arrayGlobal[0][0].'</p>';
-    echo '<p>'.$arrayGlobal[0][1].'</p>';
-    echo '<p>'.$arrayGlobal[0][2].'</p>';
-    echo '<p>'.$arrayGlobal[0][3].'</p>';
-    echo '<p>'.$arrayGlobal[1][0].'</p>';
-    echo '<p>'.$arrayGlobal[1][1].'</p>';
-    echo '<p>'.$arrayGlobal[1][2].'</p>';
-    echo '<p>'.$arrayGlobal[1][3].'</p>';
-
-    //tableau basket
-    echo '<p>'.$arrayBasket[0][0].'</p>';
-    echo '<p>'.$arrayBasket[0][1].'</p>';
-    echo '<p>'.$arrayBasket[0][2].'</p>';
-    echo '<p>'.$arrayBasket[0][3].'</p>';
-    echo '<p>'.$arrayBasket[1][0].'</p>';
-    echo '<p>'.$arrayBasket[1][1].'</p>';
-    echo '<p>'.$arrayBasket[1][2].'</p>';
-    echo '<p>'.$arrayBasket[1][3].'</p>';
-
-    //tableau foot
-    echo '<p>'.$arrayFoot[0][0].'</p>';
-    echo '<p>'.$arrayFoot[0][1].'</p>';
-    echo '<p>'.$arrayFoot[0][2].'</p>';
-    echo '<p>'.$arrayFoot[0][3].'</p>';
-    echo '<p>'.$arrayFoot[1][0].'</p>';
-    echo '<p>'.$arrayFoot[1][1].'</p>';
-    echo '<p>'.$arrayFoot[1][2].'</p>';
-    echo '<p>'.$arrayFoot[1][3].'</p>';
-    
-    //tableau rugby
-    echo '<p>'.$arrayRugby[0][0].'</p>';
-    echo '<p>'.$arrayRugby[0][1].'</p>';
-    echo '<p>'.$arrayRugby[0][2].'</p>';
-    echo '<p>'.$arrayRugby[0][3].'</p>';
-    echo '<p>'.$arrayRugby[1][0].'</p>';
-    echo '<p>'.$arrayRugby[1][1].'</p>';
-    echo '<p>'.$arrayRugby[1][2].'</p>';
-    echo '<p>'.$arrayRugby[1][3].'</p>';
-
-    //tableau tennis
-    echo '<p>'.$arrayTennis[0][0].'</p>';
-    echo '<p>'.$arrayTennis[0][1].'</p>';
-    echo '<p>'.$arrayTennis[0][2].'</p>';
-    echo '<p>'.$arrayTennis[0][3].'</p>';
-    echo '<p>'.$arrayTennis[1][0].'</p>';
-    echo '<p>'.$arrayTennis[1][1].'</p>';
-    echo '<p>'.$arrayTennis[1][2].'</p>';
-    echo '<p>'.$arrayTennis[1][3].'</p>';
+    $customizedArray = customizedRSSArray($megaArray, 12, 2, 0, 1);
+    // foreach ($customizedArray as $key => $value) {
+    //     echo '<p>'.$value[0].'</p>';
+    //     echo '<p>'.$value[1].'</p>';
+    //     echo '<p>'.$value[2].'</p>';
+    //     echo '<p>'.$value[3].'</p>';
+    // }
 
     include(__DIR__.'/../views/header.php');
     include(__DIR__.'/../views/pages.php');
