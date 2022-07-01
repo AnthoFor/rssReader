fed9aa5093d833f10ba433471d5283776bd210b4
QuentinParsy <parsyquentin@gmail.com>
2022-07-01 15:13:01 +0200

    formcomplete

--- views/parameters.php
+++ views/parameters.php
@@ -13,41 +13,58 @@
 
 <body>
 
-    <div class="container-fluid">
-        <div class="container">
-            <form class="row justify-content-center">
-                <label class="parameters text-center mb-5 mt-5 fw-bolder" for="parameters">PARAMÈTRES</label>
-                <div class="col-12 mb-3">
+    <div class="container-fluid rounded w-50">
+        <div class="container d-flex justify-content-center align-items-center vh-100 vw-90">
+            <form class="row justify-content-center rounded">
+                <label class="parameters text-center fw-bolder mb-5 mt-3" for="parameters">PARAMÈTRES</label>
+                <div class="col-12 d-flex justify-content-center align-items-center mb-5">
+                    <label class="form-check-label"></label>
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="Football">
-                        <label class="form-check-label" for="flexSwitchCheckChecked">Thème</label>
+                        <input class="form-check-input" type="checkbox" id="darkMode">
+                        <label class="form-check-label" for="">Thème sombre</label>
                     </div>
                 </div>
                 <div class="col-auto">
-                    <label for="flux" class="mb-3 fw-bold">Vos préférences de flux</label>
+                    <label for="flux" class="fw-bold mb-2">Vos préférences de flux</label>
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="generalNews">
-                        <label class="form-check-label" for="flexSwitchCheckDefault">Actualités générales</label>
+                        <input class="form-check-input" name="categories" type="checkbox" id="generalNews">
+                        <label class="form-check-label" for="generalNews">Actualités générales</label>
                     </div>
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="Football">
-                        <label class="form-check-label" for="flexSwitchCheckChecked">Football</label>
+                        <input class="form-check-input" name="categories" type="checkbox" id="football">
+                        <label class="form-check-label" for="football">Football</label>
                     </div>
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="Tennis">
-                        <label class="form-check-label" for="flexSwitchCheckChecked">Tennis</label>
+                        <input class="form-check-input" name="categories" type="checkbox" id="tennis">
+                        <label class="form-check-label" for="tennis">Tennis</label>
                     </div>
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="Rugby">
-                        <label class="form-check-label" for="flexSwitchCheckChecked">Rugby</label>
+                        <input class="form-check-input" name="categories" type="checkbox" id="rugby">
+                        <label class="form-check-label" for="rugby">Rugby</label>
                     </div>
                     <div class="form-check form-switch">
-                        <input class="form-check-input" type="checkbox" id="Basket">
-                        <label class="form-check-label" for="flexSwitchCheckChecked">Basket</label>
+                        <input class="form-check-input" name="categories" type="checkbox" id="basketBall">
+                        <label class="form-check-label" for="basketBall">Basket</label>
                     </div>
                 </div>
-                <div class="col-12 text-center mt-4 mb-3">
-                    <button type="submit" class="btn btn-dark">Enregistrer</button>
+
+                <label class="text-center fw-bold mt-5 mb-1" for="newsDisplay">Nombre d'actualités à afficher sur la page</label>
+                <div class="col-12 d-flex justify-content-center">
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                        <label class="form-check-label" for="inlineRadio1">6</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                        <label class="form-check-label" for="inlineRadio1">9</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                        <label class="form-check-label" for="inlineRadio1">12</label>
+                    </div>
+                </div>
+                <div class="col-12 d-flex justify-content-center">
+                    <button type="submit" class="btn btn-dark mt-3 mb-3">Enregistrer</button>
                 </div>
             </form>
         </div>
