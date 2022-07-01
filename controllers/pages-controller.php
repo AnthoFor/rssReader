aedf35c621355ec02f083db954e1faa1290bf244
Antho <maxister@gmail.com>
2022-07-01 14:58:47 +0200

    fixes

--- controllers/pages-controller.php
+++ controllers/pages-controller.php
@@ -37,31 +37,7 @@
     }
     array_push($megaArray, $arrayBasket, $arrayFoot, $arrayRugby, $arrayTennis, $arrayGlobal);
 
-    $defautArray = defaultRSSArray($arrayFoot, $arrayBasket, $arrayTennis);
 
-    foreach ($defautArray as $value) {
-        echo '<p>'.$value[0].'</p>';
-        echo '<p>'.$value[1].'</p>';
-        echo '<p>'.$value[2].'</p>';
-        echo '<p>'.$value[3].'</p>';
-    }
-
-    $cat = 'foot';
-    $oneCat = onlyOneFlux($cat, $megaArray);
-    // foreach ($oneCat as $key => $value) {
-    //     echo '<p>'.$value[0].'</p>';
-    //     echo '<p>'.$value[1].'</p>';
-    //     echo '<p>'.$value[2].'</p>';
-    //     echo '<p>'.$value[3].'</p>';
-    // }
-
-    $customizedArray = customizedRSSArray($megaArray, 12, 2, 0, 1);
-    // foreach ($customizedArray as $key => $value) {
-    //     echo '<p>'.$value[0].'</p>';
-    //     echo '<p>'.$value[1].'</p>';
-    //     echo '<p>'.$value[2].'</p>';
-    //     echo '<p>'.$value[3].'</p>';
-    // }
 
     include(__DIR__.'/../views/header.php');
     include(__DIR__.'/../views/home.php');
