b3c8137f55eb8b01254d763fcd3d7427eff1c09e
QuentinParsy <parsyquentin@gmail.com>
2022-07-04 14:21:53 +0200

    yes

--- controllers/parameters-controller.php
+++ controllers/parameters-controller.php
@@ -1,38 +1,51 @@
 <!-- Récupération des préférences users -->
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $darkMode = filter_input(INPUT_GET, 'darkMode', FILTER_SANITIZE_NUMBER_INT);
-    if (is_numeric($darkMode)) {
-        $resultDarkMode = filter_var($darkMode, FILTER_VALIDATE_BOOLEAN);
-        if (!$resultDarkMode) {
-            $errorMsgDarkMode["darkMode"] = 'Erreur de thème';
-        }
+
+if (!empty($_COOKIE['darkMode'])) {
+    $darkMode = $_COOKIE['darkMode'];
+} else {
+    $darkMode = NULL;
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $darkMode = intval(filter_input(INPUT_POST, 'darkMode', FILTER_SANITIZE_NUMBER_INT));
+    if ($darkMode != 0 && $darkMode != 1) {
+        $errorMsgDarkMode["darkMode"] = 'Erreur lors de la séléction du thème';
+    } else {
+        setcookie('darkMode', $darkMode, time() + (60*60*24*30));
     }
-    $categories = array();
 
-    (!empty($_GET['generalNews'])) ? array_push($categories, 'Actualités générales') : '';
-    (!empty($_GET['football'])) ? array_push($categories, 'Football') : '';
-    (!empty($_GET['tennis'])) ? array_push($categories, 'Tennis') : '';
-    (!empty($_GET['rugby'])) ? array_push($categories, 'Rugby') : '';
-    (!empty($_GET['basket'])) ? array_push($categories, 'Basket') : '';
+    $categories = filter_input(INPUT_POST, 'categories', FILTER_SANITIZE_SPECIAL_CHARS, FILTER_REQUIRE_ARRAY);
+    var_dump($categories);
+    if(!empty($categories)) {
+        setcookie('categories', serialize($categories), time() + (60*60*24*30));
 
-    $newsDisplay = intval(filter_input(INPUT_GET, 'newsDisplay', FILTER_SANITIZE_NUMBER_INT));
+    }
+    // $categoriesArray = array();
+    // if (count($categories) > 0 && count($categories) <= 3) {
+    //     foreach ($categories as $categorie) {
+    //         array_push($categoriesArray, $categories);
+    //     }
+    //     for ($i= 0; $i < count($categoriesArray); $i++) {
+    //         setcookie($i, $categoriesArray[$i][1], time() + (60*60*24*30)); 
+    //     }
+    //     var_dump($categoriesArray);
+    // } else {
+    //     $errorMsgCategories["categories"] = 'Vous ne pouvez séléctionner que 3 catégories';
+    // }
+
+    $newsDisplay = intval(filter_input(INPUT_POST, 'newsDisplay', FILTER_SANITIZE_NUMBER_INT));
     if (is_numeric($newsDisplay)) {
-        $resultnewsDisplay = filter_var($newsDisplay, FILTER_VALIDATE_INT);
+        $resultNewsDisplay = filter_var($newsDisplay, FILTER_VALIDATE_INT);
         if (!$resultNewsDisplay) {
             $errorMsgNewsDisplay["newsDisplay"] = 'Merci de choisir uniquement une des propositions';
         }
     }
 }
+// Gestion des cookies
 
-// <!-- Gestion des cookiess -->
 
-
-// $checkBoxArray = array();
-// if (!empty($_POST['idInput'])) {
-//     $checkBoxArray = array_push($_POST['idInput']);
-
-//     setcookie('id', $idUser, time() + (60*60*24*30), "/"); // 30 jours
-//     setcookie('pw', $pwUser, time() + (60*60*24*30), "/");
-// }
+include(__DIR__ . '/../views/header.php');
+include(__DIR__ . '/../views/parameters.php');
 ?>
