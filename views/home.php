7a56e37f72c1931e7ce4260086579ee5b6f9a93f
Antho <maxister@gmail.com>
2022-07-04 16:14:15 +0200

    fix

--- views/home.php
+++ views/home.php
@@ -1,37 +1,39 @@
 <?php
+if (!empty($_COOKIE['categories'])) {
 $intermediate = json_decode($_COOKIE['categories']);
-if (!empty($intermediate)) {   
-    $preference1 = $intermediate[0];
-    $preference2 = $intermediate[1];
-    if (count($intermediate) >= 3) {
-        $preference3 = $intermediate[2];
-    } else { $preference3 = null;}
-    $customizedArray = customizedRSSArray($megaArray, 12, 2, $preference1, $preference2, $preference3);
-    // var_dump($customizedArray);
-    foreach ($customizedArray as $key => $value) {
-        ?>
-                <div class="card mx-auto my-3 bg-white cardSizes">
-                <div class="card-body">
-                    <!-- Image -->
-                    <p class="card-text removeTxt"><?=$value[2]?></p>
-                    <!-- le Titre -->
-                    <h5 class="card-title">
-                    <?=$value[1] ?>
-                    </h5>
-                    <!-- la date -->
-                    <p>
-                    <?=$value[0]?>
-                    </p>
-                    <!-- quick description -->
-                    <p class="card-text removeImg align-self-stretch"><?=$value[2]?></p>
-                    <!-- le bouton avec le lien vers l'article -->
-                    <div class="position-relative">
-                        <a href="<?=$value[3]?>" class="btn btn-primary position-absolute bottom-0 end-0" target="_blank">Voir article</a>
+    if (!empty($intermediate)) {   
+        $preference1 = $intermediate[0];
+        $preference2 = $intermediate[1];
+        if (count($intermediate) >= 3) {
+            $preference3 = $intermediate[2];
+        } else { $preference3 = null;}
+        $customizedArray = customizedRSSArray($megaArray, 12, 2, $preference1, $preference2, $preference3);
+        // var_dump($customizedArray);
+        foreach ($customizedArray as $key => $value) {
+            ?>
+                    <div class="card mx-auto my-3 bg-white cardSizes">
+                    <div class="card-body">
+                        <!-- Image -->
+                        <p class="card-text removeTxt"><?=$value[2]?></p>
+                        <!-- le Titre -->
+                        <h5 class="card-title">
+                        <?=$value[1] ?>
+                        </h5>
+                        <!-- la date -->
+                        <p>
+                        <?=$value[0]?>
+                        </p>
+                        <!-- quick description -->
+                        <p class="card-text removeImg align-self-stretch"><?=$value[2]?></p>
+                        <!-- le bouton avec le lien vers l'article -->
+                        <div class="position-relative">
+                            <a href="<?=$value[3]?>" class="btn btn-primary position-absolute bottom-0 end-0" target="_blank">Voir article</a>
+                        </div>
+                        
                     </div>
-                    
                 </div>
-            </div>
 <?php
+        }
     }
     //Si get est empty, gogo tableau par défaut ( foot + basket + tennis)
 } elseif (empty($_GET)) {
