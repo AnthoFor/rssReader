625220056463c5a4e1b8ad6206e0cf70b22bc6e1
Thibaud Marin <thibaud.marin1@gmail.com>
2022-07-04 09:56:21 +0200

    mdfsklgflgdgg

--- views/nav.php
+++ views/nav.php
@@ -1,12 +1,12 @@
                 <!-- Nav bar en mode < 576 px  -->
 <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light text-center bg-dark navBarSm">
+    <nav class="navbar navbar-expand-lg navbar-light text-center bg-light navBarSm">
         <div class="container-fluid justify-content-around ">
-                <a  href="../views/parameters.php" class="bi bi-gear gear  text-light justify-content-start img-fluid" ></a>
+                <a  href="../views/parameters.php" class="bi bi-gear gear  text-dark justify-content-start img-fluid" ></a>
                 <a href="../controllers/pages-controller.php"><img src="../public/assets/img/lEQUIPE_22Logo.png" class="logo"  alt="logo de l'équipe 22 avec style, une chaussure multicolor"></a>
                 <div>
-                <h1 class="title text-light ">L'EQUIPE 22 </h1>
-                <h3 class="text-light slogan">L'info Sport</h3>
+                <h1 class="title text-dark ">L'EQUIPE 22 </h1>
+                <h3 class="text-dark slogan">L'info Sport</h3>
                 </div>
 
                 <button class="navbar-toggler bg-light buttonNav " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -16,11 +16,11 @@
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
                         <li class="nav-item dropdown ">
                             <ul  aria-labelledby="navbarDropdown ">
-                                <li><a class="dropdown-item text-light" href="../controllers/pages-controller.php?cat=global">Actualités générales</a></li>
-                                <li><a class="dropdown-item text-light" href="../controllers/pages-controller.php?cat=foot">Football</a></li>
-                                <li><a class="dropdown-item text-light" href="../controllers/pages-controller.php?cat=tennis">Tennis</a></li>
-                                <li><a class="dropdown-item text-light" href="../controllers/pages-controller.php?cat=rugby">Rugby</a></li>
-                                <li><a class="dropdown-item text-light" href="../controllers/pages-controller.php?cat=basket">Basket</a></li>
+                                <li><a class="dropdown-item text-dark" href="../controllers/pages-controller.php?cat=global">Actualités générales</a></li>
+                                <li><a class="dropdown-item text-dark" href="../controllers/pages-controller.php?cat=foot">Football</a></li>
+                                <li><a class="dropdown-item text-dark" href="../controllers/pages-controller.php?cat=tennis">Tennis</a></li>
+                                <li><a class="dropdown-item text-dark" href="../controllers/pages-controller.php?cat=rugby">Rugby</a></li>
+                                <li><a class="dropdown-item text-dark" href="../controllers/pages-controller.php?cat=basket">Basket</a></li>
                             </ul>
                         </li>
                     </ul>
@@ -32,23 +32,23 @@
 
                         <!-- Nav bar en mode > 576 px  -->
 <div>
-    <nav class="navbar  navbar-light bg-light text-center bg-dark navBarLg">
+    <nav class="navbar  navbar-light text-center bg-light navBarLg">
         <div class="container-fluid justify-content-around  w-100">
-            <a  href="../views/parameters.php" class="bi bi-gear gear text-light justify-content-start " ></a>
+            <a  href="../views/parameters.php" class="bi bi-gear gear text-dark justify-content-start " ></a>
         <div>
-            <h1 class="title text-light ">L'EQUIPE 22 </h1>
-            <h3 class="text-light slogan">L'info Sport</h3>
+            <h1 class="title text-dark ">L'EQUIPE 22 </h1>
+            <h3 class="text-dark slogan">L'info Sport</h3>
         </div>
         <a href="../controllers/pages-controller.php"><img src="../public/assets/img/lEQUIPE_22Logo.png" class="logo"  alt="logo de l'équipe 22 avec style, une chaussure multicolor"></a>
 
 
         </div>
-        <div class="text-light d-flex justify-content-around w-100 mt-3">
-            <p><a class="text-decoration-none text-light" href="../controllers/pages-controller.php?cat=global">Actualités générales <img src="../public/assets/img/newspaper-svgrepo-com.svg" alt="logo journal illustrant les news général" class="iconSports ms-3"></a></p>
-            <p><a class="text-decoration-none text-light" href="../controllers/pages-controller.php?cat=foot">Football <img src="../public/assets/img/football-svgrepo-com.svg" alt="logo ball de football illustrant les news de foot"  class="iconSports ms-3"></a></p>
-            <p><a class="text-decoration-none text-light" href="../controllers/pages-controller.php?cat=tennis">Tennis <img src="../public/assets/img/big-tennis-ball-svgrepo-com.svg" alt="logo ball de football illustrant les news de foot"  class="iconSports ms-3"></a></p>
-            <p><a class="text-decoration-none text-light" href="../controllers/pages-controller.php?cat=rugby">Rugby <img src="../public/assets/img/rugby-ball-svgrepo-com.svg" alt="logo ball de football illustrant les news de foot"  class="iconSports ms-3"></a></p>
-            <p><a class="text-decoration-none text-light" href="../controllers/pages-controller.php?cat=basket">Basket <img src="../public/assets/img/basketball-ball-svgrepo-com.svg" alt="logo ball de football illustrant les news de foot"  class="iconSports ms-3"></a></p>
+        <div class="text-dark d-flex justify-content-around w-100 mt-3">
+            <p><a class="text-decoration-none text-dark" href="../controllers/pages-controller.php?cat=global">Actualités générales <img src="../public/assets/img/newspaper-svgrepo-com.svg" alt="logo journal illustrant les news général" class="iconSports ms-3"></a></p>
+            <p><a class="text-decoration-none text-dark" href="../controllers/pages-controller.php?cat=foot">Football <img src="../public/assets/img/football-svgrepo-com.svg" alt="logo ball de football illustrant les news de foot"  class="iconSports ms-3"></a></p>
+            <p><a class="text-decoration-none text-dark" href="../controllers/pages-controller.php?cat=tennis">Tennis <img src="../public/assets/img/big-tennis-ball-svgrepo-com.svg" alt="logo ball de football illustrant les news de foot"  class="iconSports ms-3"></a></p>
+            <p><a class="text-decoration-none text-dark" href="../controllers/pages-controller.php?cat=rugby">Rugby <img src="../public/assets/img/rugby-ball-svgrepo-com.svg" alt="logo ball de football illustrant les news de foot"  class="iconSports ms-3"></a></p>
+            <p><a class="text-decoration-none text-dark" href="../controllers/pages-controller.php?cat=basket">Basket <img src="../public/assets/img/basketball-ball-svgrepo-com.svg" alt="logo ball de football illustrant les news de foot"  class="iconSports ms-3"></a></p>
         </div>
     </nav>
 </div>
--- views/pages.php
+++ views/pages.php
@@ -1,13 +1,27 @@
+            <!-- Exemple de card -->
+
 <div class="card bg-danger" style="width: 20rem;">
+
+            <!-- Image (la classe="card-img-top") -->
     <img src="..." class="card-img-top" alt="...">
+
+            <!-- body -->
     <div class="card-body">
 
+            <!-- le Titre -->
         <h5 class="card-title">
             <?php  ?>
         </h5>
 
+        <!-- la date -->
+        <p>
+        <?php  ?>
+        </p>
+
+        <!-- quick description -->
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <p  class="card-text"><?php $megaArray[0][$i][0] ?> </p>
+
+        <!-- le bouton avec le lien vers l'article -->
         <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
 </div>
