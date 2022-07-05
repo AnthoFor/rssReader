46299f471dae2e35bb47d6c13582cab8f4c86a80
Thibaud Marin <thibaud.marin1@gmail.com>
2022-07-05 11:35:24 +0200

    dqqdqs

--- views/header.php
+++ views/header.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>L'équipe 22 </title>
+    <meta title="description" content="L'info 100 % sport 100% gratuit pour vous tenir informé">
     <!-- bootstrap 5  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- librairie icons bootstraps -->
